736a05bdb81b1d85c222edaa0bbfd4eda87a0f12
NekoJar <mzharfanhakim7@gmail.com>
2023-11-07 10:04:08 +0700

    Bulid an API for deleting issue

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,19 +1,7 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import { IssueSchema } from "../../../validationSchema";
-import { data } from "autoprefixer";
 
-export async function POST(request: NextRequest) {
-  const body = await request.json();
-  const validation = IssueSchema.safeParse(body);
-  if (!validation.success)
-    return NextResponse.json(validation.error.format(), { status: 400 });
-  const newIssue = await prisma.issue.create({
-    data: { title: body.title, description: body.description },
-  });
-
-  return NextResponse.json(newIssue, { status: 201 });
-}
 export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } }
@@ -37,4 +25,19 @@ export async function PATCH(
   });
 
   return NextResponse.json(updatedIssue, { status: 201 });
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!issue)
+    return NextResponse.json({ error: "Invalid Issue" }, { status: 404 });
+
+  await prisma.issue.delete({ where: { id: issue.id } });
+
+  return NextResponse.json({});
 }
