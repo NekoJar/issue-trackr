2cc1a8cc23a9bb170638fe664d5e8a7e27770767
NekoJar <mzharfanhakim7@gmail.com>
2023-11-08 14:25:46 +0700

    add the login and logout links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,9 +4,12 @@ import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
 import classnames from "classnames";
+import { useSession } from "next-auth/react";
+import { Box } from "@radix-ui/themes";
 
 const NavBar = () => {
   const currentPath = usePathname();
+  const { status, data: session } = useSession();
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues/list" },
@@ -19,19 +22,38 @@ const NavBar = () => {
       </Link>
       <ul className="flex space-x-6">
         {links.map((link) => (
-          <Link
-            key={link.href}
-            href={link.href}
-            className={classnames({
-              "text-zinc-900": link.href === currentPath,
-              "text-zinc-500": link.href !== currentPath,
-              "hover:text-zinc-800 transition-colors": true,
-            })}
-          >
-            {link.label}
-          </Link>
+          <li key={link.href}>
+            <Link
+              href={link.href}
+              className={classnames({
+                "text-zinc-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
+            >
+              {link.label}
+            </Link>
+          </li>
         ))}
       </ul>
+      <Box>
+        {status === "authenticated" && (
+          <Link
+            href="api/auth/signout"
+            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+          >
+            Sign Out
+          </Link>
+        )}
+        {status === "unauthenticated" && (
+          <Link
+            href="api/auth/signin"
+            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+          >
+            Sign In
+          </Link>
+        )}
+      </Box>
     </nav>
   );
 };
